b712d4c85bf7ffe74013f983c1912de8b34df32c
小橙子 <a2742217795@hotmail.com>
2023-04-24 12:14:49 +0800

    fix: 修改 工具函数

--- utils/index.js
+++ utils/index.js
@@ -1,36 +1,27 @@
-export const FULFILLED = "fulfilled"
-export const REJECTED = "rejected"
-export const PENDING = "pending"
+const FULFILLED = "fulfilled"
+const REJECTED = "rejected"
+const PENDING = "pending"
 
 
-export function isFunction(func) {
-  if (typeof func === 'function') {
+const isFunction = obj => typeof obj === 'function'
+const isObject = obj => !!(obj && typeof obj === 'object')
+const isThenable = obj => (isFunction(obj) || isObject(obj)) && 'then' in obj
+
+
+const isIterator = (iterator) => {
+  if (iterator && isFunction(iterator[Symbol.iterator])) {
     return true
   }
 
   return false
 }
 
-export function isThenable(obj) {
-  if (obj && typeof obj === 'object' && 'then' in obj) {
-    return true
-  }
+module.exports = {
+  FULFILLED,
+  REJECTED,
+  PENDING,
 
-  false
-}
-
-export function isPromise(promise) {
-  if ('state' in promise) {
-    return promise.state === PENDING || FULFILLED || REJECTED
-  }
-
-  return false
-}
-
-export function isIterator(iterator) {
-  if (iterator && typeof iterator[Symbol.iterator] === 'function') {
-    return true
-  }
-
-  return false
+  isFunction,
+  isThenable,
+  isIterator,
 }
