eaf207325003ee34987038bb1735cd5cc0abb52d
小橙子 <a2742217795@hotmail.com>
2023-04-28 13:45:26 +0800

    feat: add Promise.any

---
+++ src/any.js
@@ -0,0 +1,45 @@
+const { isIterator } = require('../utils')
+const Promise = require('./promise')
+
+
+module.exports = Promise
+
+// 参考 https://es6.ruanyifeng.com/#docs/promise#Promise-any 
+/**
+ * 接收一个 iterate 任何一个 为 resolve 那返回 promise 状态为 resolve 
+ * 全部的 promise 状态为 rected 时 返回 rejected(AggreagateError(errors)) 
+ * @param {*} iterator 
+ * @returns 
+ */
+Promise.prototype.any = function (iterator) {
+  const rejecteds = []
+
+  return Promise((resolve, reject) => {
+    if (!isIterator(iterator)) {
+      const result = new TypeError('iterator is no Iterator type')
+      return reject(result)
+    }
+    const length = iterator.length
+    const onFulfilled = value => {
+      resolve(value)
+    }
+
+    const onRejected = reason => {
+      rejecteds.push(reason)
+
+      if (rejecteds.length === length) {
+        // @see https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/AggregateError
+        if(AggregateError){
+          return reject(new AggregateError(rejecteds))
+        }
+        reject(rejecteds)
+      }
+    }
+
+    for (const promise of iterator) {
+      if (promise instanceof Promise) {
+        promise.then(onFulfilled, onRejected)
+      }
+    }
+  })
+}
